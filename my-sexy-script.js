868ba97a078c16721769dc92e69a91b075b034a7
David Young-Chan Kay <davidykay@gmail.com>
2012-09-29 15:49:33 -0400

    Basic HTML/CSS/JS lesson

---
+++ my-sexy-script.js
@@ -0,0 +1,13 @@
+
+/*
+ * jquery can be used as either "jQuery" or "$"
+ * Hence, jQuery.ready(...); and $.ready(...) are the SAME!!!!
+ */
+
+jQuery(document).ready(
+    function () { // THIS IS A LAMBDA!!!!!!!!
+      alert("Hello world!!!!");
+    }
+);
+
+//alert("Hello world!!!!");
